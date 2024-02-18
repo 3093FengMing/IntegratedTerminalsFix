f9c33f04c762a0d856a478d3eb9f1e863d284e62
3093FengMing <83573615+3093FengMing@users.noreply.github.com>
2024-02-18 14:54:09 +0800

    3

--- src/main/java/me/fengming/integratedterminalsfix/IntegratedTerminalsFix.java
+++ src/main/java/me/fengming/integratedterminalsfix/IntegratedTerminalsFix.java
@@ -6,14 +6,4 @@ import org.apache.logging.log4j.Logger;
 
 @Mod("integratedterminalsfix")
 public class IntegratedTerminalsFix {
-    private static final Logger LOGGER = LogManager.getLogger();
-
-    public IntegratedTerminalsFix() {https://github.com/3093FengMing/IntegratedTerminalsFix
-        LOGGER.info("[IntegratedTerminalsFix] Author: FengMing");
-        LOGGER.info("[IntegratedTerminalsFix] Github: https://github.com/3093FengMing/IntegratedTerminalsFix");
-    }
-
-    public static void loaded() {
-        LOGGER.info("[IntegratedTerminalsFix] Successfully fixed bug.");
-    }
 }
