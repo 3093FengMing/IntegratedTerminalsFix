1394d42a1b22c07ea7d892d3c6a654409591ccbf
3093FengMing <83573615+3093FengMing@users.noreply.github.com>
2024-02-18 14:44:44 +0800

    2

--- src/main/java/me/fengming/integratedterminalsfix/mixin/MixinTerminalStorageIngredientItemStackCraftingGridClear.java
+++ src/main/java/me/fengming/integratedterminalsfix/mixin/MixinTerminalStorageIngredientItemStackCraftingGridClear.java
@@ -1,2 +1,23 @@
-package me.fengming.integratedterminalsfix.mixin;public class MixinTerminalStorageIngredientItemStackCraftingGridClear {
+package me.fengming.integratedterminalsfix.mixin;
+
+import me.fengming.integratedterminalsfix.IntegratedTerminalsFix;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.CraftingInventory;
+import net.minecraft.item.ItemStack;
+import org.cyclops.integratedterminals.core.terminalstorage.TerminalStorageTabIngredientComponentItemStackCraftingCommon;
+import org.cyclops.integratedterminals.core.terminalstorage.TerminalStorageTabIngredientComponentServer;
+import org.cyclops.integratedterminals.network.packet.TerminalStorageIngredientItemStackCraftingGridClear;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+@Mixin(TerminalStorageIngredientItemStackCraftingGridClear.class)
+public class MixinTerminalStorageIngredientItemStackCraftingGridClear {
+    @Inject(method = "clearGrid", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerInventory;placeItemBackInInventory(Lnet/minecraft/world/World;Lnet/minecraft/item/ItemStack;)V", shift = At.Shift.AFTER), locals = LocalCapture.CAPTURE_FAILHARD)
+    private static void inject_clearGrid_fixDupeItems(TerminalStorageTabIngredientComponentItemStackCraftingCommon tabCommon, TerminalStorageTabIngredientComponentServer<ItemStack, Integer> tabServer, int channel, boolean toStorage, PlayerEntity player, CallbackInfo ci, CraftingInventory inventoryCrafting, int i, ItemStack itemStack, ItemStack remainder) {
+        inventoryCrafting.setInventorySlotContents(i, ItemStack.EMPTY);
+        IntegratedTerminalsFix.loaded();
+    }
 }
