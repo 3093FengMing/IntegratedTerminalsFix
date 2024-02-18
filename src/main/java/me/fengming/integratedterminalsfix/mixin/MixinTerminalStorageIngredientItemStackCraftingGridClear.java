29571b10713a558dcc30025eeace9c14189ddfb5
3093FengMing <83573615+3093FengMing@users.noreply.github.com>
2024-02-18 15:25:29 +0800

    d

--- src/main/java/me/fengming/integratedterminalsfix/mixin/MixinTerminalStorageIngredientItemStackCraftingGridClear.java
+++ src/main/java/me/fengming/integratedterminalsfix/mixin/MixinTerminalStorageIngredientItemStackCraftingGridClear.java
@@ -3,20 +3,34 @@ package me.fengming.integratedterminalsfix.mixin;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.CraftingInventory;
 import net.minecraft.item.ItemStack;
-import org.apache.logging.log4j.LogManager;
 import org.cyclops.integratedterminals.core.terminalstorage.TerminalStorageTabIngredientComponentItemStackCraftingCommon;
 import org.cyclops.integratedterminals.core.terminalstorage.TerminalStorageTabIngredientComponentServer;
 import org.cyclops.integratedterminals.network.packet.TerminalStorageIngredientItemStackCraftingGridClear;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+import org.spongepowered.asm.mixin.Overwrite;
 
 @Mixin(TerminalStorageIngredientItemStackCraftingGridClear.class)
 public class MixinTerminalStorageIngredientItemStackCraftingGridClear {
-    @Inject(method = "clearGrid", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerInventory;placeItemBackInInventory(Lnet/minecraft/world/World;Lnet/minecraft/item/ItemStack;)V", shift = At.Shift.AFTER), locals = LocalCapture.CAPTURE_FAILHARD)
-    private static void inject_clearGrid_fixDupeItems(TerminalStorageTabIngredientComponentItemStackCraftingCommon tabCommon, TerminalStorageTabIngredientComponentServer<ItemStack, Integer> tabServer, int channel, boolean toStorage, PlayerEntity player, CallbackInfo ci, CraftingInventory inventoryCrafting, int i, ItemStack itemStack, ItemStack remainder) {
-        inventoryCrafting.setInventorySlotContents(i, ItemStack.EMPTY);
+    /**
+     * @author 3093FengMing
+     * @reason Fix items dupe bug
+     */
+    @Overwrite(remap = false)
+    public static void clearGrid(TerminalStorageTabIngredientComponentItemStackCraftingCommon tabCommon, TerminalStorageTabIngredientComponentServer<ItemStack, Integer> tabServer, int channel, boolean toStorage, PlayerEntity player) {
+        tabCommon.getInventoryCraftResult().setInventorySlotContents(0, ItemStack.EMPTY);
+        CraftingInventory inventoryCrafting = tabCommon.getInventoryCrafting();
+
+        for (int i = 0; i < inventoryCrafting.getSizeInventory(); ++i) {
+            ItemStack itemStack = inventoryCrafting.removeStackFromSlot(i);
+            if (!itemStack.isEmpty()) {
+                if (toStorage) {
+                    ItemStack remainder = tabServer.getIngredientNetwork().getChannel(channel).insert(itemStack, false);
+                    inventoryCrafting.setInventorySlotContents(i, remainder);
+                } else {
+                    player.inventory.placeItemBackInInventory(player.world, itemStack);
+                    inventoryCrafting.setInventorySlotContents(i, ItemStack.EMPTY);
+                }
+            }
+        }
     }
 }
